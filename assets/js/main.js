07bde02a2b3d07fdfdc9dda01bb7a95542769c75
unknown <irfanullahkk715@gmail.com>
2021-10-03 12:35:30 +0500

    asdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,19 @@ $(function() {
             margin: 20,
             smartSpeed: 1000,
             autoplay: true,
-            nav: false,
+            nav: true,
             dots: true,
             pauseOnHover: false,
             responsive:{
                 600:{
                     margin: 20,
-                    nav: false,
+                    nav: true,
                   items: 2
                 },
                 1000:{
                     margin: 20,
                     stagePadding: 0,
-                    nav: false,
+                    nav: true,
                   items: 3
                 }
             }
@@ -43,19 +43,19 @@ $(function() {
             margin: 20,
             smartSpeed: 1000,
             autoplay: true,
-            nav: false,
+            nav: true,
             dots: true,
             pauseOnHover: false,
             responsive:{
                 600:{
                     margin: 20,
-                    nav: false,
+                    nav: true,
                   items: 2
                 },
                 1000:{
                     margin: 20,
                     stagePadding: 0,
-                    nav: false,
+                    nav: true,
                   items: 2
                 }
             }
